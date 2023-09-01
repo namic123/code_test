de523d161f849e1e1fb9ff015bd2678771457401
jay sung <namic123@naver.com>
2023-09-01 18:02:15 +0900

    코드 테스트 레벨 1

--- src/codetest/lv1/CodeTest09.java
+++ src/codetest/lv1/CodeTest09.java
@@ -1,34 +1,36 @@
 package codetest.lv1;
 
 public class CodeTest09 {
+    private static String babblingFilter(String str){
+        if(str.contains("aya")||str.contains("ye")||str.contains("ma")||str.contains("woo")) {
+            for (int i = 0; i < str.length(); i++) {
+                        if (str.contains("1aya")) break;
+                        str = str.replaceFirst("aya", "1");
+
+                        if (str.contains("2ye")) break;
+                        str = str.replaceFirst("ye", "2");
+
+                       if (str.contains("3ma")) break;
+                        str = str.replaceFirst("ma", "3");
+
+                        if (str.contains("4woo")) break;
+                        str = str.replaceFirst("woo", "4");
+                    }
+        }
+        return str;
+    }
     public int solution(String[] babbling) {
         int result = 0;
         for(int i=0; i<babbling.length; i++){
-            babbling[i] = babbling[i].replaceFirst("aya","!");
-            if(!babbling[i].contains("!aya")){
-                while (babbling[i].contains("aya")) {
-                    babbling[i] = babbling[i].replaceFirst("aya", "!");
-                }
+            babbling[i] = babbling[i].replaceFirst("aya","1");
+            babbling[i] = babbling[i].replaceFirst("ye","2");
+            babbling[i] = babbling[i].replaceFirst("ma","3");
+            babbling[i] = babbling[i].replaceFirst("woo","4");
+
+            babbling[i] = babblingFilter(babbling[i]);
+            for(int j=1; j<5; j++) {
+                babbling[i] = babbling[i].replaceAll(String.valueOf(j), "");
             }
-            babbling[i] = babbling[i].replaceFirst("ye","!");
-            if(!babbling[i].contains("!ye")){
-                while (babbling[i].contains("ye")) {
-                    babbling[i] = babbling[i].replaceFirst("ye", "!");
-                }
-            }
-            babbling[i] = babbling[i].replaceFirst("ma","!");
-            if(!babbling[i].contains("!ma")){
-                while (babbling[i].contains("ma")) {
-                    babbling[i] = babbling[i].replaceFirst("ma", "!");
-                }
-            }
-            babbling[i] = babbling[i].replaceFirst("woo","!");
-            if(!babbling[i].contains("!woo")){
-                while (babbling[i].contains("woo")) {
-                    babbling[i] = babbling[i].replaceFirst("woo", "!");
-                }
-            }
-            babbling[i] = babbling[i].replaceAll("!","");
             if(babbling[i].equals("")){result++;}
         }
         return result;
---
+++ src/codetest/lv1/CodeTest10.java
@@ -0,0 +1,16 @@
+package codetest.lv1;
+
+import java.util.HashMap;
+
+public class CodeTest10 {
+//    public String[] solution(String[] players, String[] callings) {
+//        HashMap<Integer, String> playersMap = new HashMap<Integer, String>();
+//        String tmp;
+//        for (int i = 0; i < players.length; i++) {
+//            playersMap.put(i,players[i]);
+//        }
+//        for(int i=0; i <callings.length; i++){
+//        }
+//
+//    }
+}
